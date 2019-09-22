258cb69cbf600159878721d108112bddb09ecf54
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 23:34:18 -0400

    Update andrewnav.php

--- inc/andrewnav.php
+++ inc/andrewnav.php
@@ -1,7 +1,7 @@
 <nav class="menu">
 
 	<ul>
-		<li><a href="home.php">Andrew</a></li>
+		<li><a href="andrew-home.php">Andrew</a></li>
 		<li><a href="andrew-earlydays.php">Early Days</a></li>
 		<li><a href="andrew-collegelife.php">College Life</a></li>
 		<li><a href="andrew-futureplans.php">Future Plans</a></li>
@@ -15,11 +15,11 @@
 
 			<li><a href="index.php">Home</a></li>
 
-			<li><a href="../alyssa/home.php">Alyssa Nelson</a></li>
+			<li><a href="alyssa-home.php" class="is-current">Alyssa Nelson</a></li>
 
-			<li><a href="../maria/home.php">Maria Mancheno</a></li>
+			<li><a href="maria-home.php">Maria Mancheno</a></li>
 
-			<li><a href="../andrew/home.php" class="is-current">Andrew Thankson</a></li>
+			<li><a href="andrew-home.php">Andrew Thankson</a></li>
 
 		</ul>
 </nav>
