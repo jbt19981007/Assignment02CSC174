94c4e5e389f442374aa103af2351acfd3e3bf89b
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 23:00:49 -0400

    Update alyssanav.php

--- inc/alyssanav.php
+++ inc/alyssanav.php
@@ -15,7 +15,7 @@
 
 			<li><a href="index.php">Home</a></li>
 
-			<li><a href="alyssa/home.php">Alyssa Nelson</a></li>
+			<li><a href="alyssa/home.php" class="is-current">Alyssa Nelson</a></li>
 
 			<li><a href="maria/home.php">Maria Mancheno</a></li>
 
