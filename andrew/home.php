3e8d013b0c4333a3683464c46ff31bd7dbc738be
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 21:44:50 -0400

    Update and rename index.php to home.php

--- andrew/home.php
+++ andrew/home.php
@@ -2,7 +2,7 @@
 <?php include "inc/html-top.php"; ?>
 <body class="container">
 <header >
-	<a href="andrew-home.php">
+	<a href="home.php">
 		<h1>Andrew Thankson</h1>
         <img src="images/andrew.jpg" alt="Andrew Thankson Picture">
     </a>
