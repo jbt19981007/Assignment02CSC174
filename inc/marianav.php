260adbcdae821a8f69706140bdf72adb31ad915c
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 21:20:01 -0400

    Update marianav.php

--- inc/marianav.php
+++ inc/marianav.php
@@ -4,7 +4,7 @@
 		<li><a href="maria-home.php">Maria</a></li>
 		<li><a href="maria-highschool.php">Highschool</a></li>
 		<li><a href="maria-hobbies.php">Hobbies</a></li>
-		<li><a href="maria-unversity.php">University</a></li>
+		<li><a href="maria-university.php">University</a></li>
 
 
 	</ul>
