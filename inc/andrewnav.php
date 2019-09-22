206997df9d93dd46f49846c0276e981a3a9ffb9f
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 23:40:42 -0400

    Update andrewnav.php

--- inc/andrewnav.php
+++ inc/andrewnav.php
@@ -15,11 +15,11 @@
 
 			<li><a href="index.php">Home</a></li>
 
-			<li><a href="alyssa-home.php" class="is-current">Alyssa Nelson</a></li>
+			<li><a href="alyssa-home.php">Alyssa Nelson</a></li>
 
 			<li><a href="maria-home.php">Maria Mancheno</a></li>
 
-			<li><a href="andrew-home.php">Andrew Thankson</a></li>
+			<li><a href="andrew-home.php" class="is-current">Andrew Thankson</a></li>
 
 		</ul>
 </nav>
