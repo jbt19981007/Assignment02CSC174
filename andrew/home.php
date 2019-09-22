aa48dd8368cb424c6cdaedc3f5c56c37d004f4cd
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 21:56:15 -0400

    Update home.php

--- andrew/home.php
+++ andrew/home.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<?php include "inc/html-top.php"; ?>
+<?php include "../inc/html-top.php"; ?>
 <body class="container">
 <header >
 	<a href="home.php">
 		<h1>Andrew Thankson</h1>
-        <img src="images/andrew.jpg" alt="Andrew Thankson Picture">
+        <img src="../images/andrew.jpg" alt="Andrew Thankson Picture">
     </a>
 </header>
 <aside>
@@ -14,8 +14,8 @@
 	<li>Entrepreneur</li>
 	</ul>
 </aside>
-<?php include "inc/nav.php"; ?>
-<?php include "inc/andrewnav.php"; ?>
+<?php include "../inc/nav.php"; ?>
+<?php include "../inc/andrewnav.php"; ?>
 
 <main>
 	<p>I am currently a second-year student at the University of Rochester in Rochester, New York. I am thinking of majoring in Computer Science with a double minor in Philosophy and Psychology. I am originally from Ghana, where I grew up and schooled for most of my life. I moved to Rochester in the United States just a bit over a year ago to start my College Education. I founded a non-profit organization called Speak to the Nation Inc. in my home country Ghana to help kids from rural areas of the country learn more about the importance of Education by sharing personal experiences and also encouraging them through motivational messages and donating stationary to help them in their journey as students. I also started out as a freelance fronted web developer about a year or two ago. I have worked with a wide range of people in developing websites, applications, teaching students programming, and in other jobs such as being an office assistant and program assistant.</p>
@@ -24,7 +24,7 @@
 </main>
 
 <footer >CSC 174: Advanced Front-end Web Design and Development</footer>
-<?php include "inc/scripts.php";?>
+<?php include "../inc/scripts.php";?>
   
 </body>
 
