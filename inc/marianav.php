1592122963fa150144813d828f0915c81d30d069
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 23:35:11 -0400

    Update marianav.php

--- inc/marianav.php
+++ inc/marianav.php
@@ -1,7 +1,7 @@
 <nav class="menu">
 
 	<ul>
-		<li><a href="home.php">Maria</a></li>
+		<li><a href="maria-home.php">Maria</a></li>
 		<li><a href="maria-highschool.php">Highschool</a></li>
 		<li><a href="maria-hobbies.php">Hobbies</a></li>
 		<li><a href="maria-university.php">University</a></li>
@@ -15,11 +15,11 @@
 
 			<li><a href="index.php">Home</a></li>
 
-			<li><a href="../alyssa/home.php">Alyssa Nelson</a></li>
+			<li><a href="alyssa-home.php">Alyssa Nelson</a></li>
 
-			<li><a href="../maria/home.php" class="is-current">Maria Mancheno</a></li>
+			<li><a href="maria-home.php" class="is-current">Maria Mancheno</a></li>
 
-			<li><a href="../andrew/home.php">Andrew Thankson</a></li>
+			<li><a href="andrew-home.php">Andrew Thankson</a></li>
 
 		</ul>
 </nav>
