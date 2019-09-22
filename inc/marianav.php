c131aee60f314fd0a935b2250b98fdd8038530de
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 23:45:05 -0400

    Update marianav.php

--- inc/marianav.php
+++ inc/marianav.php
@@ -1,16 +1,4 @@
 <nav class="menu">
-
-	<ul>
-		<li><a href="maria-home.php">Maria</a></li>
-		<li><a href="maria-highschool.php">Highschool</a></li>
-		<li><a href="maria-hobbies.php">Hobbies</a></li>
-		<li><a href="maria-university.php">University</a></li>
-
-
-	</ul>
-
-</nav>
-<nav class="menu">
 	<ul>
 
 			<li><a href="index.php">Home</a></li>
@@ -23,3 +11,16 @@
 
 		</ul>
 </nav>
+<nav class="menu">
+
+	<ul>
+		<li><a href="maria-home.php">Maria</a></li>
+		<li><a href="maria-highschool.php">Highschool</a></li>
+		<li><a href="maria-hobbies.php">Hobbies</a></li>
+		<li><a href="maria-university.php">University</a></li>
+
+
+	</ul>
+
+</nav>
+
