2968c9892c3a8750255b49f5f8b20270364fe00b
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 21:43:40 -0400

    Update andrewnav.php

--- inc/andrewnav.php
+++ inc/andrewnav.php
@@ -1,7 +1,7 @@
 <nav class="menu">
 
 	<ul>
-		<li><a href="andrew/index.php">Andrew</a></li>
+		<li><a href="andrew/home.php">Andrew</a></li>
 		<li><a href="andrew/andrew-earlydays.php">Early Days</a></li>
 		<li><a href="andrew/andrew-collegelife.php">College Life</a></li>
 		<li><a href="andrew/andrew-futureplans.php">Future Plans</a></li>
