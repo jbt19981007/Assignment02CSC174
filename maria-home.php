442af67d871506eacef2df66cf924cbdcc4789a2
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 23:31:11 -0400

    Update and rename maria/home.php to maria-home.php

--- maria-home.php
+++ maria-home.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<?php include "../inc/html-top.php"; ?>
+<?php include "inc/html-top.php"; ?>
 <body class="container">
 <header >
 	<a href="home.php">
 		<h1>Maria Mancheno</h1>
-        <img src="../images/maria.jpg" alt="Maria Mancheno Picture">
+        <img src="images/maria.jpg" alt="Maria Mancheno Picture">
     </a>
 </header>
 <aside>
@@ -14,7 +14,7 @@
 	<li>Digital Media Studies</li>
 	</ul>
 </aside>
-<?php include "../inc/marianav.php"; ?>
+<?php include "inc/marianav.php"; ?>
 
 <main>
 	<p>My name is Maria Mancheno and I’m from Quito, Ecuador. I’m 21 years old and I currently go to the University of Rochester. My major is in Digital Media Studies and I’m also doing a minor in Math. My hobbies include playing soccer, playing the guitar and reading. I work as a social media manager and web developer at the Peak Lab in the University of Rochester Medical Center. This last summer I had the opportunity to do an internship in an Ecuadorian firm where I was able to improve my programming skills. My favorite thing is to travel around the world to learn about new cultures and meet new interesting people.</p>
@@ -23,7 +23,7 @@
 </main>
 
 <footer >CSC 174: Advanced Front-end Web Design and Development</footer>
-<?php include "../inc/scripts.php";?>
+<?php include "inc/scripts.php";?>
   
 </body>
 
