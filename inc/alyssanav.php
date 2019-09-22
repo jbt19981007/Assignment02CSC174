bbad32be33e9674dea28754badb6cbee27000aa2
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 21:31:54 -0400

    Update alyssanav.php

--- inc/alyssanav.php
+++ inc/alyssanav.php
@@ -1,10 +1,10 @@
 <nav class="menu">
 
 	<ul>
-		<li><a href="alyssa-home.php">Alyssa</a></li>
-		<li><a href="alyssa-highschool.php">Highschool</a></li>
-		<li><a href="alyssa-earlycollege.php">Early College</a></li>
-		<li><a href="alyssa-now.php">Now</a></li>
+		<li><a href="alyssa/index.php">Alyssa</a></li>
+		<li><a href="alyssa/alyssa-highschool.php">Highschool</a></li>
+		<li><a href="alyssa/alyssa-earlycollege.php">Early College</a></li>
+		<li><a href="alyssa/alyssa-now.php">Now</a></li>
 
 
 	</ul>
