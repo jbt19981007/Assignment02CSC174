79ad4d637e2044dda9d0ba6df0d977b21931a296
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 23:01:16 -0400

    Update marianav.php

--- inc/marianav.php
+++ inc/marianav.php
@@ -10,3 +10,16 @@
 	</ul>
 
 </nav>
+<nav class="menu">
+	<ul>
+
+			<li><a href="index.php">Home</a></li>
+
+			<li><a href="alyssa/home.php">Alyssa Nelson</a></li>
+
+			<li><a href="maria/home.php" class="is-current">Maria Mancheno</a></li>
+
+			<li><a href="andrew/home.php">Andrew Thankson</a></li>
+
+		</ul>
+</nav>
