4f5e78b2bc1624aff4d49f001ad565a437924dc3
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 23:18:48 -0400

    Update alyssanav.php

--- inc/alyssanav.php
+++ inc/alyssanav.php
@@ -1,10 +1,10 @@
 <nav class="menu">
 
 	<ul>
-		<li><a href="alyssa/home.php">Alyssa</a></li>
-		<li><a href="alyssa/alyssa-highschool.php">Highschool</a></li>
-		<li><a href="alyssa/alyssa-earlycollege.php">Early College</a></li>
-		<li><a href="alyssa/alyssa-now.php">Now</a></li>
+		<li><a href="home.php">Alyssa</a></li>
+		<li><a href="alyssa-highschool.php">Highschool</a></li>
+		<li><a href="alyssa-earlycollege.php">Early College</a></li>
+		<li><a href="alyssa-now.php">Now</a></li>
 
 
 	</ul>
@@ -15,11 +15,11 @@
 
 			<li><a href="index.php">Home</a></li>
 
-			<li><a href="alyssa/home.php" class="is-current">Alyssa Nelson</a></li>
+			<li><a href="../alyssa/home.php" class="is-current">Alyssa Nelson</a></li>
 
-			<li><a href="maria/home.php">Maria Mancheno</a></li>
+			<li><a href="../maria/home.php">Maria Mancheno</a></li>
 
-			<li><a href="andrew/home.php">Andrew Thankson</a></li>
+			<li><a href="../andrew/home.php">Andrew Thankson</a></li>
 
 		</ul>
 </nav>
