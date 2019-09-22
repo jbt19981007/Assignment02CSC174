70aed464ce4f3d6c2a1f98e98df6663762f93118
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-21 21:57:35 -0400

    Update maria-highschool.php

--- maria/maria-highschool.php
+++ maria/maria-highschool.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<?php include "inc/html-top.php"; ?>
+<?php include "../inc/html-top.php"; ?>
 <body class= "container">
-<?php include "inc/nav.php"; ?>
-<?php include "inc/marianav.php"; ?>
+<?php include "../inc/nav.php"; ?>
+<?php include "../inc/marianav.php"; ?>
 <main>
 		<h1>Highschool</h1>
 
