f087ea361bb2b822a68170a4409b706e7652ce2f
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-23 17:10:37 -0400

    Add files via upload

---
+++ inc/andrewnav.php
@@ -0,0 +1,26 @@
+<nav class="home menu">
+	<ul>
+
+			<li><a href="index.php">Home</a></li>
+
+			<li><a href="alyssa-home.php">Alyssa Nelson</a></li>
+
+			<li><a href="maria-home.php">Maria Mancheno</a></li>
+
+			<li><a href="andrew-home.php" class="is-current">Andrew Thankson</a></li>
+
+		</ul>
+</nav>
+<nav class="sub menu">
+
+	<ul>
+		<li><a href="andrew-home.php">Andrew</a></li>
+		<li><a href="andrew-earlydays.php">Early Days</a></li>
+		<li><a href="andrew-collegelife.php">College Life</a></li>
+		<li><a href="andrew-futureplans.php">Future Plans</a></li>
+
+
+	</ul>
+
+</nav>
+
